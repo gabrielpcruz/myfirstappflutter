3054d8a1eff4b6d253d431d241af70b1d35fc9e7
Gabriel Cruz <gabrielcruz.mail@gmail.com>
2022-12-26 09:13:54 -0300

    Adding files

---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.red,
+                  height: 100,
+                  width: 100,
+                ),
+                Container(
+                  color: Colors.blue,
+                  height: 50,
+                  width: 50,
+                ),
+              ],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.blue,
+                  height: 100,
+                  width: 100,
+                ),
+                Container(
+                  color: Colors.red,
+                  height: 50,
+                  width: 50,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  color: Colors.red,
+                  height: 100,
+                  width: 100,
+                ),
+                Container(
+                  color: Colors.blue,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.amber,
+                  height: 50,
+                  width: 50,
+                ),
+              ],
+            ),
+            Container(
+              color: Colors.amber,
+              height: 30,
+              width: 300,
+              child: const Text(
+                'Texto de demonstração',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print("arroz");
+              },
+              child: const Text('Aperte o botão!'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
